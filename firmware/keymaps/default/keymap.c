07c936b17af67468d9c8f283c7f99ba59c4b6bbe
Taksh Nahata <w2154626@apps.losrios.edu>
2026-05-29 13:47:39 -0700

    Add 3-key QMK firmware source code

---
+++ firmware/keymaps/default/keymap.c
@@ -0,0 +1,15 @@
+// Copyright 2026 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┬───┐
+     * │ 1 │ 2 │ 3 │
+     * └───┴───┴───┘
+     */
+    [0] = LAYOUT_ortho_1x3(
+        KC_A, KC_B, KC_C
+    )
+};
